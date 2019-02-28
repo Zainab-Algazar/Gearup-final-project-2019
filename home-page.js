57853a4d2bae53e9001f88bc7952bc6406c428a1
fatmaMGomaa <fatma-mohamed@feps.edu.eg>
2019-02-28 14:21:14 +0200

    adding objects to js

--- home-page.js
+++ home-page.js
@@ -2,4 +2,67 @@ $(document).ready(function(){
     $(".search-icon").click(function(){
         $(".form-style").style.display = "block";
     })
+    var album1={
+    	"id": "album_1",
+    	"name": "Pink Friday",
+    	"photo": "http://theaudacityofcolor.com/core/wp-content/uploads/2010/10/Nicki-Minaj-Pink-Friday.jpg",
+    	"category":"Albums",
+    	"price": 60
+    };
+    var album2={
+    	"id": "album_2",
+    	"name": "LandMark Void",
+    	"photo": "http://idaaf.com/wp-content/uploads/2014/03/3.Landmark-VOID.jpg",
+    	"category":"Albums",
+    	"price": 50
+    };
+    var album3={
+    	"id": "album_3",
+    	"name": "Danger Doom",
+    	"photo": "https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/6b2ef062-6913-4a94-a265-7d75f4f91854/64.jpg",
+    	"category":"Albums",
+    	"price": 50
+    };
+    var book1={
+    	"id": "book_1",
+    	"name": "Speak",
+    	"photo": "https://images.gr-assets.com/books/1529044298l/39280444.jpg",
+    	"category":"Books",
+    	"price": 50
+    };
+    var book2={
+    	"id": "book_2",
+    	"name": "A Dolly's House",
+    	"photo": "https://images.gr-assets.com/books/1427119396l/37793.jpg",
+    	"category":"Books",
+    	"price": 70
+    };
+    var book3={
+    	"id": "book_3",
+    	"name": "Little Women",
+    	"photo": "https://images.gr-assets.com/books/1388269517l/1934.jpg",
+    	"category":"Books",
+    	"price": 35
+    };
+    var movie1={
+    	"id": "movie_1",
+    	"name": "Maiden",
+    	"photo": "https://previews-graphicriver.imgix.net/files/228254188/movie%20posters%20poster%20template%208.5x11.jpg?auto=compress%2Cformat&fit=crop&crop=top&max-h=8000&max-w=590&s=2f070615ecf33c60d0e1df0ddcaf552d",
+    	"category":"Movies",
+    	"price": 20
+    };
+    var movie2={
+    	"id": "movie_2",
+    	"name": "Right Time",
+    	"photo": "https://marketplace.canva.com/MACGp9_jvD0/2/0/thumbnail_large/canva-cityscape-buildings-movie-poster-MACGp9_jvD0.jpg",
+    	"category":"Movies",
+    	"price": 50
+    };
+    var movie3={
+    	"id": "movie_3",
+    	"name": "What They Had",
+    	"photo": "https://www.joblo.com/assets/images/oldsite/posters/images/full/WHATTHEYHAD_Poster.jpg",
+    	"category":"Movies",
+    	"price": 85
+    };
     });
