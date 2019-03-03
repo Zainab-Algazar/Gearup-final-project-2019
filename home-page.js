80e36602d8c744ed0bc64783f6c0f02cd87142bf
fatmaMGomaa <fatma-mohamed@feps.edu.eg>
2019-03-03 12:44:47 +0200

    product object and editing filter function

--- home-page.js
+++ home-page.js
@@ -3,11 +3,12 @@ $(document).ready(function(){
     $(".search-icon").click(function(){
     $(".searchbar-style").slideToggle("500");
     });
-    $(".cart-icon").click(function(){
-    $(".shopping-cart").slideToggle("500");
-    $(".shopping-cart").css({display:'flex',flexDirection: 'column'})
-    });
-    var album_array =[{
+    // $(".cart-icon").click(function(){
+    // $(".shopping-cart").slideToggle("500");
+    // $(".shopping-cart").css({display:'flex',flexDirection: 'column'})
+    // });
+    var product = {
+        "album_array": [{
         "id": "album_1",
         "name": "Pink Friday",
         "photo": "http://theaudacityofcolor.com/core/wp-content/uploads/2010/10/Nicki-Minaj-Pink-Friday.jpg",
@@ -25,9 +26,8 @@ $(document).ready(function(){
         "photo": "https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/6b2ef062-6913-4a94-a265-7d75f4f91854/64.jpg",
         "category":"Albums",
         "price": 75
-    }];
-    
-    var book_array=[{
+    }],
+        "book_array": [{
         "id": "book_1",
         "name": "Speak",
         "photo": "https://images.gr-assets.com/books/1529044298l/39280444.jpg",
@@ -45,9 +45,8 @@ $(document).ready(function(){
         "photo": "https://images.gr-assets.com/books/1388269517l/1934.jpg",
         "category":"Books",
         "price": 35
-    }];
-
-    var movie_array=[{
+    }],
+        "movie_array": [{
         "id": "movie_1",
         "name": "Maiden",
         "photo": "https://previews-graphicriver.imgix.net/files/228254188/movie%20posters%20poster%20template%208.5x11.jpg?auto=compress%2Cformat&fit=crop&crop=top&max-h=8000&max-w=590&s=2f070615ecf33c60d0e1df0ddcaf552d",
@@ -65,8 +64,8 @@ $(document).ready(function(){
         "photo": "https://www.joblo.com/assets/images/oldsite/posters/images/full/WHATTHEYHAD_Poster.jpg",
         "category":"Movies",
         "price": 85
-    }];
-    
+    }]
+    };
     function addToPage(a){
         for(var j=0; j< a.length;j++){
         var i="#"+a[j].id;
@@ -85,9 +84,9 @@ $(document).ready(function(){
         $(p).text(a[j].price + " EGP");
         }
     };
-        addToPage(album_array);
-        addToPage(book_array);
-        addToPage(movie_array);
+        addToPage(product.album_array);
+        addToPage(product.book_array);
+        addToPage(product.movie_array);
     
     var productList = ["All", "Albums", "Books","Movies"];
     var j=1;
@@ -102,22 +101,24 @@ $(document).ready(function(){
       var passOption= $(event.target).val();
       switch(passOption){
         case "1":
-            $('.grid-container').empty();
-            addToPage(album_array);
-            addToPage(book_array);
-            addToPage(movie_array);
+            $('.grid-container .Albums').show();
+            $('.grid-container .Books').show();
+            $('.grid-container .Movies').show();
             break;
         case "2":
-            $('.grid-container').empty();
-            addToPage(album_array);
+            $('.grid-container .Albums').show();
+            $('.grid-container .Books').hide();
+            $('.grid-container .Movies').hide();
             break;
         case "3":
-            $('.grid-container').empty();
-            addToPage(book_array);
+            $('.grid-container .Books').show();
+            $('.grid-container .Albums').hide();
+            $('.grid-container .Movies').hide();
             break;
         case "4":
-            $('.grid-container').empty();
-            addToPage(movie_array);
+            $('.grid-container .Movies').show();
+            $('.grid-container .Albums').hide();
+            $('.grid-container .Books').hide();
             break;
       };
     });
