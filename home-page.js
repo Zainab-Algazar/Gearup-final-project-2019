a335c1e5496c6e78d1478b8ddec32ff32f28eb20
fatmaMGomaa <fatma-mohamed@feps.edu.eg>
2019-03-03 17:49:36 +0200

    first draft for search

--- home-page.js
+++ home-page.js
@@ -9,62 +9,62 @@ $(document).ready(function(){
     // });
     var product = {
         "album_array": [{
-        "id": "album_1",
-        "name": "Pink Friday",
-        "photo": "http://theaudacityofcolor.com/core/wp-content/uploads/2010/10/Nicki-Minaj-Pink-Friday.jpg",
-        "category":"Albums",
-        "price": 60
-    },{
-        "id": "album_2",
-        "name": "LandMark Void",
-        "photo": "http://idaaf.com/wp-content/uploads/2014/03/3.Landmark-VOID.jpg",
-        "category":"Albums",
-        "price": 50
-    },{
-        "id": "album_3",
-        "name": "Danger Doom",
-        "photo": "https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/6b2ef062-6913-4a94-a265-7d75f4f91854/64.jpg",
-        "category":"Albums",
-        "price": 75
-    }],
+            "id": "album_1",
+            "name": "Pink Friday",
+            "photo": "http://theaudacityofcolor.com/core/wp-content/uploads/2010/10/Nicki-Minaj-Pink-Friday.jpg",
+            "category":"Albums",
+            "price": 60
+         },{
+            "id": "album_2",
+            "name": "LandMark Void",
+            "photo": "http://idaaf.com/wp-content/uploads/2014/03/3.Landmark-VOID.jpg",
+            "category":"Albums",
+            "price": 50
+        },{
+            "id": "album_3",
+            "name": "Danger Doom",
+            "photo": "https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/6b2ef062-6913-4a94-a265-7d75f4f91854/64.jpg",
+            "category":"Albums",
+            "price": 75
+        }],
         "book_array": [{
-        "id": "book_1",
-        "name": "Speak",
-        "photo": "https://images.gr-assets.com/books/1529044298l/39280444.jpg",
-        "category":"Books",
-        "price": 50
-    },{
-        "id": "book_2",
-        "name": "A Dolly's House",
-        "photo": "https://images.gr-assets.com/books/1427119396l/37793.jpg",
-        "category":"Books",
-        "price": 70
-    },{
-        "id": "book_3",
-        "name": "Little Women",
-        "photo": "https://images.gr-assets.com/books/1388269517l/1934.jpg",
-        "category":"Books",
-        "price": 35
-    }],
+            "id": "book_1",
+            "name": "SPEAK",
+            "photo": "https://images.gr-assets.com/books/1529044298l/39280444.jpg",
+            "category":"Books",
+            "price": 50
+        },{
+            "id": "book_2",
+            "name": "A Dolly's House",
+            "photo": "https://images.gr-assets.com/books/1427119396l/37793.jpg",
+            "category":"Books",
+            "price": 70
+        },{
+            "id": "book_3",
+            "name": "Little Women",
+            "photo": "https://images.gr-assets.com/books/1388269517l/1934.jpg",
+            "category":"Books",
+            "price": 35
+        }],
         "movie_array": [{
-        "id": "movie_1",
-        "name": "Maiden",
-        "photo": "https://previews-graphicriver.imgix.net/files/228254188/movie%20posters%20poster%20template%208.5x11.jpg?auto=compress%2Cformat&fit=crop&crop=top&max-h=8000&max-w=590&s=2f070615ecf33c60d0e1df0ddcaf552d",
-        "category":"Movies",
-        "price": 20
-    },{
-        "id": "movie_2",
-        "name": "Right Time",
-        "photo": "https://marketplace.canva.com/MACGp9_jvD0/2/0/thumbnail_large/canva-cityscape-buildings-movie-poster-MACGp9_jvD0.jpg",
-        "category":"Movies",
-        "price": 50
-    },{
-        "id": "movie_3",
-        "name": "What They Had",
-        "photo": "https://www.joblo.com/assets/images/oldsite/posters/images/full/WHATTHEYHAD_Poster.jpg",
-        "category":"Movies",
-        "price": 85
-    }]
+            "id": "movie_1",
+            "name": "Maiden",
+            "photo": "https://previews-graphicriver.imgix.net/files/228254188/movie%20posters%20poster%20template%208.5x11.jpg?auto=compress%2Cformat&fit=crop&crop=top&max-h=8000&max-w=590&s=2f070615ecf33c60d0e1df0ddcaf552d",
+            "category":"Movies",
+            "price": 20
+        },{
+            "id": "movie_2",
+            "name": "Right Time",
+            "photo": "https://marketplace.canva.com/MACGp9_jvD0/2/0/thumbnail_large/canva-cityscape-buildings-movie-poster-MACGp9_jvD0.jpg",
+            "category":"Movies",
+            "price": 50
+        },{
+            "id": "movie_3",
+            "name": "What They Had",
+            "photo": "https://www.joblo.com/assets/images/oldsite/posters/images/full/WHATTHEYHAD_Poster.jpg",
+            "category":"Movies",
+            "price": 85
+        }]
     };
     function addToPage(a){
         for(var j=0; j< a.length;j++){
@@ -96,7 +96,25 @@ $(document).ready(function(){
            $('select option:last-child').text(productList[i]);
           j+=1;
     }
+    $("#searchbar").on("keyup", function(event){
+      var userInput= $(event.target).val().toUpperCase();
+      for(var key in product){
+        for(var i=0;i<product[key].length;i++){
+            var nameOfPuoduct= product[key][i]["name"].toUpperCase();
+            
+            if(nameOfPuoduct.includes(userInput)==true){
+                var searchArray=[];
+                searchArray.push(product[key][i]);
+                $('.grid-container').empty();
+                addToPage(searchArray);
+            }else if(userInput=="" || nameOfPuoduct.includes(userInput)==false){
 
+                $('.grid-container section').show();
+            };
+        };
+      };
+      
+    });
     $("select").change (function(event){
       var passOption= $(event.target).val();
       switch(passOption){
